e11e623d2ec8659d5fbf6111c2ef51ced53e5507
GomDiing <gomdiing@gmail.com>
2022-09-15 17:44:24 +0900

    Add Thread 5

---
+++ src/쓰레드복습5/AutoSaveTh.java
@@ -0,0 +1,14 @@
+package 쓰레드복습5;
+
+public class AutoSaveTh extends Thread{
+    @Override
+    public void run() {
+        while (true) {
+            System.out.println("작업 내용을 저장 함...");
+            try {
+                sleep(1000);
+            } catch (InterruptedException e) {
+            }
+        }
+    }
+}
